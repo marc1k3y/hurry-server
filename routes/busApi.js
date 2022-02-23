dad2d2a14b88290b7b8df4fbac8f64631f73d21b
hubs0ne <hubsonemain@gmail.com>
2022-02-23 23:23:53 +0300

    try query search

--- routes/busApi.js
+++ routes/busApi.js
@@ -151,7 +151,7 @@ router.get('/menuPage', (req, res) => {
 })
 
 router.get('/queryShops', (req, res) => {
-  const { query } = req.body
+  const { query } = req.query
   Bus.find({ "info.title": { "$regex": query, "$options": "i" } })
     .then((buses) => {
       res.send(buses)
